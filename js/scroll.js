ae1c842a6138c646ea3d7565051e7b5a633303a5
qinzengfu <15938365700@163.com>
2017-01-05 10:19:32 +0800

    添加scroll事件

--- js/scroll.js
+++ js/scroll.js
@@ -1,37 +1,41 @@
-var startPosition, endPosition, startTime, endTime, deltaY, flag=false;
+var startPosition, endPosition, startTime, endTime, deltaY, deltaX, flag=false;
 var scrollt,
-	screen_width= window.screen.width,
-	screen_height= window.screen.height;
+	screen_width = window.screen.width,
+	screen_height= window.screen.height,
+	width5       =screen_height*5;
+
+$("body").css("width",screen_width)
+
+$(".top").css({"width":width5+"px","height":screen_height});
+$(".top div").css({"height":screen_height,"width":screen_width})
 
 
+//$(".top video").css({"height":screen_height})
 
-var str="";
-var curr_j="";
 for(var j=0;j<5;j++){
-	curr_j="movies/"+(j+1)+".mp4";
-	str+='<video src='+curr_j+' controls></video>';
-}
-$(".example").html(str)
-
-for(var j=0;j<5;j++){
-	console.log($(".example video").eq(j).css("width"))
+	console.log($(".video-wrap video").eq(j).selector)
+	console.log($(".video-wrap video").eq(j).selector[0].readyState);
+	console.log($(".video-wrap video").eq(j).selector[0].src);
+	console.log($(".video-wrap video").eq(j).selector[0].preload);
+	console.log($(".video-wrap video").eq(j)["0"].videoWidth)
 }
 
-$(".top").css({"width":screen_width,"height":screen_height});
+
+
+
+
 
 
 $(window).resize(function(){
 	screen_width= window.screen.width;
 	screen_height= window.screen.height;
 	console.log(screen_height,screen_width);
-	$(".top").css({"width":screen_width,"height":screen_height});
 })
 
 
 $("body").on('touchstart', function (e) {
 	scrollt=$("body").scrollTop();
 	startTime=e.timeStamp;
-	console.log("satrt");
 	flag=false;
     var touch = e.touches[0];
     startPosition = {
@@ -41,24 +45,25 @@ $("body").on('touchstart', function (e) {
 });
 
 $("body").on('touchmove', function (e) {
-	console.log("move")
     var touch = e.touches[0];
     endPosition = {
         x: touch.pageX,
         y: touch.pageY
     }
     deltaY = endPosition.y - startPosition.y;
-    
+    deltaX = endPosition.x - startPosition.x;
 });
 
 $("body").on('touchend', function (e) {
-	client_height=parseFloat(screen_height);
 	
+	
+	client_height=parseFloat(screen_height);
 	endTime=e.timeStamp;
 	if(endTime-startTime>=750){
 		flag=true;
 	}
-	console.log("end");
+	
+	
 	var curr_scroll=$("body").scrollTop();
 	if(flag==false){
     		if(deltaY<0){
